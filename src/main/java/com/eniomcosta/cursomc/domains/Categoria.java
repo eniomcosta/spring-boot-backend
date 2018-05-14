f0df40b0f853bd94c9551b2b050e1ed3add710d8
Enio Costa <enio.m.costa@gmail.com>
2018-05-14 08:57:50 -0300

    Added categoria resource.

--- src/main/java/com/eniomcosta/cursomc/domains/Categoria.java
+++ src/main/java/com/eniomcosta/cursomc/domains/Categoria.java
@@ -1,4 +1,4 @@
-package com.eniomcosta.cursomc.domain;
+package com.eniomcosta.cursomc.domains;
 
 import java.io.Serializable;
 
--- src/main/java/com/eniomcosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/eniomcosta/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,38 @@
 package com.eniomcosta.cursomc.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eniomcosta.cursomc.domain.Categoria;
+import com.eniomcosta.cursomc.domains.Categoria;
+import com.eniomcosta.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok(obj);
 		
-		List<Categoria> lista = new ArrayList<>();
+	}
+	
+	@RequestMapping(value="/all",method=RequestMethod.GET)
+	public ResponseEntity<?> findAll(){
+		List<Categoria> obj = service.findAll();
 		
-		lista.add(cat1);
-		lista.add(cat2);
+		return ResponseEntity.ok(obj);
 		
-		return lista;
 	}
 	
 }
