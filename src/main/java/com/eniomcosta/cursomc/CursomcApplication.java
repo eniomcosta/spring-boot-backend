eaaf161557fe5691a5d3d2e01e2d41213e64cc67
Enio Costa <enio@sloth5447.sloth5447.com>
2018-05-15 10:08:34 -0300

    Implemented seeds to Categoria.

--- src/main/java/com/eniomcosta/cursomc/CursomcApplication.java
+++ src/main/java/com/eniomcosta/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.eniomcosta.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.eniomcosta.cursomc.domains.Categoria;
+import com.eniomcosta.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository catRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catRepo.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 	
 	
