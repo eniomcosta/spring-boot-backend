24558eee882040451a4c7ec3d7787d521d766c52
Enio Costa <eniomcosta@sloth5447.sloth5447.com>
2018-06-13 13:32:12 -0300

    Added pagination endpoint to Categoria.

--- src/main/java/com/eniomcosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/eniomcosta/cursomc/resources/CategoriaResource.java
@@ -5,18 +5,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eniomcosta.cursomc.domains.Categoria;
 import com.eniomcosta.cursomc.dtos.CategoriaDTO;
 import com.eniomcosta.cursomc.services.CategoriaService;
-import com.eniomcosta.cursomc.services.exception.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -65,6 +66,18 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
